ce31f64b7d2f876b1b6ab5eda3da3b98c1b05c9b
Josh McArthur <joshua.mcarthur@gmail.com>
2017-06-08 22:17:20 +1200

    Add test asserting that webview is rendered on home screen

--- app/src/androidTest/java/NZSLDictionaryTest.java
+++ app/src/androidTest/java/NZSLDictionaryTest.java
@@ -1,3 +1,5 @@
+import android.content.Intent;
+import android.support.test.espresso.web.webdriver.Locator;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -6,6 +8,7 @@ import com.hewgill.android.nzsldict.NZSLDictionary;
 import com.hewgill.android.nzsldict.R;
 
 import org.hamcrest.Matcher;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,6 +22,11 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.web.assertion.WebViewAssertions.webMatches;
+import static android.support.test.espresso.web.sugar.Web.onWebView;
+import static android.support.test.espresso.web.webdriver.DriverAtoms.findElement;
+import static android.support.test.espresso.web.webdriver.DriverAtoms.getText;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.core.AllOf.allOf;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
@@ -28,7 +36,18 @@ import static org.hamcrest.Matchers.not;
 @LargeTest
 public class NZSLDictionaryTest {
     @Rule
-    public ActivityTestRule<NZSLDictionary> mActivityRule = new ActivityTestRule<>(NZSLDictionary.class);
+    public ActivityTestRule<NZSLDictionary> mActivityRule = new ActivityTestRule<NZSLDictionary>(NZSLDictionary.class, false, false) {
+        @Override
+        protected void afterActivityLaunched() {
+            // Enable JS!
+            onWebView(withId(R.id.about_content)).forceJavascriptEnabled();
+        }
+    };
+
+    @Before
+    public void startActivity() {
+        mActivityRule.launchActivity(new Intent());
+    }
 
     @Test
     public void test_wordOfTheDay() {
@@ -67,6 +86,13 @@ public class NZSLDictionaryTest {
         // TODO JM: Test should also assert that handshape view is present, but I cannot get
         // http://google.github.io/android-testing-support-library/docs/espresso/advanced/#matching-a-view-that-is-a-footerheader-in-a-listview
         // to work
+    }
+
+    @Test
+    public void test_aboutWebviewRenders() {
+        onWebView(withId(R.id.about_content))
+                .withElement(findElement(Locator.TAG_NAME, "body"))
+                .check(webMatches(getText(), containsString("NZSL Dictionary Official App")));
     }
 
 }
