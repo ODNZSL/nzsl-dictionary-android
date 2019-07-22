6cc24cca0253056c4b5e84d7734e0559b69016ba
Josh McArthur <joshua.mcarthur@gmail.com>
2019-07-22 16:40:22 +1200

    Add stub to indicate that the download has completed; this needs to be raised to the favourites activity

--- app/src/main/java/com/hewgill/android/nzsldict/DownloadReceiver.java
+++ app/src/main/java/com/hewgill/android/nzsldict/DownloadReceiver.java
@@ -4,6 +4,7 @@ import android.app.DownloadManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class DownloadReceiver extends BroadcastReceiver {
 
@@ -14,6 +15,12 @@ public class DownloadReceiver extends BroadcastReceiver {
             Intent startIntent = new Intent(context, FavouritesActivity.class);
             startIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
             context.startActivity(startIntent);
+            return;
+        }
+
+        if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
+            Toast.makeText(context, "Download completed?", Toast.LENGTH_SHORT).show();
+
         }
     }
 }
