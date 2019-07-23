2077e050e93007a12b1a4ad2a2af8f56e3299496
Josh McArthur <joshua.mcarthur@gmail.com>
2019-07-23 13:35:12 +1200

    Reimplement DownloadReceiver to follow the same pattern as that used for NetworkManager

--- app/src/main/java/com/hewgill/android/nzsldict/DownloadReceiver.java
+++ app/src/main/java/com/hewgill/android/nzsldict/DownloadReceiver.java
@@ -4,23 +4,47 @@ import android.app.DownloadManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.widget.Toast;
 
-public class DownloadReceiver extends BroadcastReceiver {
+public class DownloadReceiver {
+    private DownloadCallback callback;
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        String action = intent.getAction();
-        if (DownloadManager.ACTION_NOTIFICATION_CLICKED.equals(action)) {
-            Intent startIntent = new Intent(context, FavouritesActivity.class);
-            startIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-            context.startActivity(startIntent);
-            return;
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (DownloadManager.ACTION_NOTIFICATION_CLICKED.equals(action)) {
+                Intent startIntent = new Intent(context, FavouritesActivity.class);
+                startIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                context.startActivity(startIntent);
+                return;
+            }
+
+            if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
+                if (callback != null) callback.onDownloadCompleted();
+            }
         }
+    };
 
-        if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
-            Toast.makeText(context, "Download completed?", Toast.LENGTH_SHORT).show();
 
-        }
+
+    public void registerContext(Context context, DownloadCallback callback) {
+        this.callback = callback;
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(DownloadManager.ACTION_NOTIFICATION_CLICKED);
+        filter.addAction(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+        context.registerReceiver(receiver, filter);
+    }
+
+    public void unregisterContext(Context context) {
+        callback = null;
+        context.unregisterReceiver(receiver);
+    }
+
+    public interface DownloadCallback {
+        void onDownloadCompleted();
     }
 }
