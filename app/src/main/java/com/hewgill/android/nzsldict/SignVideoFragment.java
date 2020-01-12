0e36e717738c9d24a875658604761b8e654bd612
DarkMatterMatt <mattmoran2000@gmail.com>
2020-01-12 17:27:14 +1300

    MIgrate to Androidx

--- app/src/main/java/com/hewgill/android/nzsldict/SignVideoFragment.java
+++ app/src/main/java/com/hewgill/android/nzsldict/SignVideoFragment.java
@@ -1,33 +1,21 @@
 package com.hewgill.android.nzsldict;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
-import android.view.KeyEvent;
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.MediaController;
-import android.widget.RelativeLayout;
 import android.widget.VideoView;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 
 /**
--- app/src/main/java/com/hewgill/android/nzsldict/WordActivity.java
+++ app/src/main/java/com/hewgill/android/nzsldict/WordActivity.java
@@ -2,11 +2,11 @@ package com.hewgill.android.nzsldict;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import com.google.android.material.tabs.TabLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 import android.widget.TextView;
 
 import java.util.ArrayList;
